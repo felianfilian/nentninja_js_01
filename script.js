2c8a50099b1e7d919af4c69b98c9514f752bd944
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-02-24 13:07:44 +0100

    concat method

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ console.log(myName.toLocaleUpperCase());
 console.log(myName.slice(-5));
 console.log(myName.substring(6, 9));
 console.log(myName.replace("luigi", "peach"));
+
+// string concat
+myScore = "1250";
+
+console.log(myScore.concat(" points"));
