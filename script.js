c85ed39e2820bfd1bf85c16dcd2af87af22c8d22
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-02-25 08:40:16 +0100

    inline concat

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // string tests
 let myName = "mario and luigi";
-console.log(myName[3]);
-console.log(myName.length);
-console.log(myName.toLocaleUpperCase());
-console.log(myName.slice(-5));
-console.log(myName.substring(6, 9));
-console.log(myName.replace("luigi", "peach"));
+// console.log(myName[3]);
+// console.log(myName.length);
+// console.log(myName.toLocaleUpperCase());
+// console.log(myName.slice(-5));
+// console.log(myName.substring(6, 9));
+// console.log(myName.replace("luigi", "peach"));
 
 // string concat
 myScore = "1250";
-
 console.log(myScore.concat(" points"));
+console.log(`Score: ${myScore}`);
